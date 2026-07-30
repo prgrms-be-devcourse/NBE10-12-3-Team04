293ede9111dea4df7197480572c139f3c0bdd9b2
hannahyang116 <hannayang116@gmail.com>
2026-07-30 16:50:50 +0900

    feat: 검색 관련 DTO 추가

---
+++ back/src/main/java/com/triptrace/domain/trip/trip/dto/TripSearchCondition.java
@@ -0,0 +1,15 @@
+package com.triptrace.domain.trip.trip.dto;
+
+import java.util.List;
+
+// 검색 조건 생성
+public record TripSearchCondition (
+    List<String> tokens,
+    TripSearchScope scope,
+    String country,
+    String city
+    ){
+    public  boolean hasKeyword(){
+        return tokens != null && !tokens.isEmpty();
+    }
+}
