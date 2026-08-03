64a94366a6d029e1e22af4eb92ebbe0fd24c7245
hannahyang116 <hannayang116@gmail.com>
2026-08-03 16:10:08 +0900

    refactor: 마커 외부 연동 코틀린 마이그레이션

---
+++ back/src/main/java/com/triptrace/domain/marker/marker/geocoding/ReverseGeocodingClient.kt
@@ -0,0 +1,10 @@
+package com.triptrace.domain.marker.marker.geocoding
+
+import java.math.BigDecimal
+
+fun interface ReverseGeocodingClient {
+    fun findPlaceName(latitude: BigDecimal?, longitude: BigDecimal?): String?
+
+    fun findLocation(latitude: BigDecimal?, longitude: BigDecimal?): ReverseGeocodingResult? =
+        ReverseGeocodingResult(null, null, findPlaceName(latitude, longitude))
+}
