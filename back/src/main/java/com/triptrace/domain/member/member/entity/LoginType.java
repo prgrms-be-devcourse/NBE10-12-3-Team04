2d3bf6eb1d5b064e7a28b9e5828d4f885342653a
henry <wi990415@gmail.com>
2026-08-04 03:39:04 +0900

    feat: LoginType enum 추가

---
+++ back/src/main/java/com/triptrace/domain/member/member/entity/LoginType.java
@@ -0,0 +1,9 @@
+package com.triptrace.domain.member.member.entity;
+
+// 회원이 어떤 경로로 가입했는지. LOCAL은 자체 회원가입(이메일+비밀번호), 나머지는 소셜 로그인이다.
+public enum LoginType {
+    LOCAL,
+    GOOGLE,
+    KAKAO,
+    NAVER
+}
