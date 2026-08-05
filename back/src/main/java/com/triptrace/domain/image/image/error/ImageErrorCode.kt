378d30cd7ef4a75a3fd464b8b3e72344d5d0ae26
whitelinkblaze <whitelinkblaze@gmail.com>
2026-08-05 12:50:39 +0900

    refactor: error code 의존성 수정

--- back/src/main/java/com/triptrace/domain/image/image/error/ImageErrorCode.kt
+++ back/src/main/java/com/triptrace/domain/image/image/error/ImageErrorCode.kt
@@ -5,20 +5,20 @@ import com.triptrace.global.error.DefaultErrorCode
 import com.triptrace.global.error.ErrorCode
 
 enum class ImageErrorCode(private val code: String, private val message: String) : ErrorCode {
-    INVALID(DefaultErrorCode.BAD_REQUEST.code, DefaultErrorCode.BAD_REQUEST.message),
-    INVALID_TRIP(DefaultErrorCode.BAD_REQUEST.code, "해당 여행기의 이미지가 아닙니다."),
-    INVALID_POST(DefaultErrorCode.BAD_REQUEST.code, "해당 포스트의 이미지가 아닙니다."),
-    FAIL_SAVE(DefaultErrorCode.BAD_REQUEST.code, "이미지 파일 저장에 실패했습니다."),
-    NO_IMAGE(DefaultErrorCode.BAD_REQUEST.code, "업로드할 이미지가 없습니다."),
-    NOT_FOUND(DefaultErrorCode.NOT_FOUND.code, "이미지를 찾을 수 없습니다."),
-    FORBIDDEN(DefaultErrorCode.FORBIDDEN.code, DefaultErrorCode.FORBIDDEN.message),
-    IMAGE_PROCESSING_ERROR(DefaultErrorCode.BAD_REQUEST.code, "이미지를 읽을 수 없습니다."),
-    TYPE_ERROR(DefaultErrorCode.BAD_REQUEST.code, "파일 유형이 올바르지 않습니다."),
-    SAVE_ERROR(DefaultErrorCode.BAD_REQUEST.code, "파일을 저장할 수 없습니다."),
-    DELETE_ERROR(DefaultErrorCode.BAD_REQUEST.code, "파일 삭제를 실패했습니다."),
-    READ_ERROR(DefaultErrorCode.BAD_REQUEST.code, "이미지를 읽을 수 없습니다."),
-    FILE_EXTRACT_ERROR(DefaultErrorCode.BAD_REQUEST.code, "메타데이터를 추출할 수 없습니다."),
-    REWARD_TRANSACTION_ERROR(DefaultErrorCode.BAD_REQUEST.code, "보상 트랜잭션 실패했습니다.");
+    INVALID("400", "올바르지 않은 요청입니다."),
+    INVALID_TRIP("400", "해당 여행기의 이미지가 아닙니다."),
+    INVALID_POST("400", "해당 포스트의 이미지가 아닙니다."),
+    FAIL_SAVE( "400", "이미지 파일 저장에 실패했습니다."),
+    NO_IMAGE( "400", "업로드할 이미지가 없습니다."),
+    NOT_FOUND("404", "이미지를 찾을 수 없습니다."),
+    FORBIDDEN("403", "권한이 없습니다."),
+    IMAGE_PROCESSING_ERROR( "400", "이미지를 읽을 수 없습니다."),
+    TYPE_ERROR( "400", "파일 유형이 올바르지 않습니다."),
+    SAVE_ERROR( "400", "파일을 저장할 수 없습니다."),
+    DELETE_ERROR( "400", "파일 삭제를 실패했습니다."),
+    READ_ERROR( "400", "이미지를 읽을 수 없습니다."),
+    FILE_EXTRACT_ERROR( "400", "메타데이터를 추출할 수 없습니다."),
+    REWARD_TRANSACTION_ERROR( "400", "보상 트랜잭션 실패했습니다.");
     override fun getCode() = code
     override fun getMessage() = message
     override fun getDomain() = Domain.IMAGE
